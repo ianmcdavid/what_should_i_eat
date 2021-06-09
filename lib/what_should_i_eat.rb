6bddba912099c16f6ab980f14e297f0e9e9a699f
ianmcdavid <ianmcdavid25@gmail.com>
2021-06-09 11:01:21 -0700

    Integrated API

--- lib/what_should_i_eat.rb
+++ lib/what_should_i_eat.rb
@@ -1,6 +1,16 @@
 require "what_should_i_eat/version"
+require "http"
+require "json"
+require "what_should_i_eat/printing"
 
 module WhatShouldIEat
   class Error < StandardError; end
-  # Your code goes here...
-end
+    def WhatShouldIEat.decide
+      result = HTTP.get('https://www.edamam.com/search?type=Feeds').to_s
+      recipes = JSON.parse(result)
+      try = recipes.first['items'].first['label']
+      url = recipes.first['items'].first['url']
+      Printing.suggest try, url
+      end
+  end
+
---
+++ lib/what_should_i_eat/printing.rb
@@ -0,0 +1,8 @@
+module WhatShouldIEat
+    module Printing
+      def Printing.suggest(try, url)
+        puts try
+        puts url
+      end
+    end
+  end
